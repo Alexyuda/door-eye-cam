97bda06c66884ccc4240fe47c7b19915624c46c2
alexyuda <alexyuda@gmail.com>
2021-07-09 19:43:02 +0300

    delete old files thread refactor

--- main.py
+++ main.py
@@ -13,44 +13,31 @@ import threading
 import time
 from datetime import datetime
 
-def set_interval(interval):
-    def decorator(function):
-        def wrapper(*args, **kwargs):
-            stopped = threading.Event()
-
-            def loop(): # executed in another thread
-                while not stopped.wait(interval): # until stopped
-                    function(*args, **kwargs)
-
-            t = threading.Thread(target=loop)
-            t.daemon = True # stop if the program exits
-            t.start()
-            return stopped
-        return wrapper
-    return decorator
+DAY_IN_SECONDS = 24 * 60 * 60
 
 
-@set_interval(3600)
 def monitor_and_delete_old_videos(folder, delete_after_n_days):
-    print('entered...')
-    files = [join(folder, f) for f in listdir(folder) if isfile(join(folder, f)) and ".mp4" in f]
-    for file in files:
-        last_updated_time = datetime.fromtimestamp(os.path.getmtime(file))
-        dt_days = (datetime.now() - last_updated_time).total_seconds()/(24 * 60 * 60)
-        if dt_days > delete_after_n_days:
-            os.remove(file)
+    while True:
+        files = [join(folder, f) for f in listdir(folder) if isfile(join(folder, f)) and ".mp4" in f]
+        for file in files:
+            last_updated_time = datetime.fromtimestamp(os.path.getmtime(file))
+            dt_days = (datetime.now() - last_updated_time).total_seconds() / DAY_IN_SECONDS
+            if dt_days > delete_after_n_days:
+                os.remove(file)
+        time.sleep(DAY_IN_SECONDS)
 
 
 def main(args):
-    monitor_and_delete_old_videos(args.vid_dump_dir, args.delete_after_n_days)
+    t = threading.Thread(target=monitor_and_delete_old_videos, args=(args.vid_dump_dir, args.delete_after_n_days))
+    t.start()
 
     capture_video = CaptureVideo(int(args.input) if args.input.isdigit() else args.input, args.select_roi)
     detect_motion = DetectMotion(args.p_min_motion_area)
     detect_faces = DetectFaces(prototxt=args.prototxt, model=args.model, confidence=args.confidence,
                                batch_size=args.batch_size)
-    annotate_image = AnnotateImage("annotated_image") if args.display or args.out_video else None
-    display_video = DisplayVideo("annotated_image") if args.display else None
-    record_video = RecordVideo(args.vid_dump_dir, args.min_motion_to_save_video_sec, args.min_vid_length_sec)
+    annotate_image = AnnotateImage("image") if args.annotate else None
+    display_video = DisplayVideo("image") if args.display else None
+    record_video = RecordVideo("image", args.vid_dump_dir, args.min_motion_to_save_video_sec, args.min_vid_length_sec, args.close_cap_and_start_new_one_after_n_secs)
 
     # Create image processing pipeline
     pipeline = (capture_video |
